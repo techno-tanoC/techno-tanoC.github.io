bf2ce6dc8ca6de48a5331a2e9016b221b5651be6
techno-tanoC <develop.tanoc@gmail.com>
2024-03-06 14:02:24 +0900

    Define the posts collection

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { z, defineCollection } from "astro:content"
+
+const postsCollection = defineCollection({
+    type: "content",
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+    })
+})
+
+export const collections = {
+    posts: postsCollection,
+}
